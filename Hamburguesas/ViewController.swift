d69dde819840d3a7cd696a38c788d5f877ffc5f4
Virginia García Tomé <Virginia@macbook-air-de-virginia.local>
2016-11-07 00:16:30 +0100

    App Hamburguesas

--- Hamburguesas/ViewController.swift
+++ Hamburguesas/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pais: UILabel!
+    
+    @IBOutlet weak var tipoHamburguesa: UILabel!
+    
+    let paisRand = ColeccionDePaises()
+    let hamburguesaRand = ColeccionDeHamburguesa()
+    
+    let colores = Colores()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+
+    @IBAction func quieroUnaHamburguesa() {
+        pais.text = paisRand.obtenPais()
+        tipoHamburguesa.text = hamburguesaRand.obtenHamburguesa()
+        
+        let colorAleatorio = colores.devuelveColorAleatorio()
+        view.backgroundColor = colorAleatorio
+        view.tintColor = colorAleatorio
+    }
+    
+    
 }
 
